70a8eeb32b0a4b9d7de5bf99f3d7c86c7b172ce2
Piotr Koźbiał <piotr.jan.kozbial@gmail.com>
2023-11-13 22:24:58 +0100

    all 4 tests provided in assignment

--- src/test/java/org/example/BookingOptimizerTest.java
+++ src/test/java/org/example/BookingOptimizerTest.java
@@ -4,8 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class BookingOptimizerTest {
     @Test
@@ -21,25 +25,59 @@ class BookingOptimizerTest {
 	assertEquals(expectedResult, actualResult);
     }
 
-    @Test
-    void clientTest1() {
-	var clientOffers = Arrays.asList(
-            new BookingOptimizer.EuroAmount(23, 0),
-            new BookingOptimizer.EuroAmount(45, 0),
-            new BookingOptimizer.EuroAmount(155, 0),
-            new BookingOptimizer.EuroAmount(374, 0),
-            new BookingOptimizer.EuroAmount(22, 0),
-            new BookingOptimizer.EuroAmount(99, 99),
-            new BookingOptimizer.EuroAmount(100, 0),
-            new BookingOptimizer.EuroAmount(101, 0),
-            new BookingOptimizer.EuroAmount(115, 0),
-            new BookingOptimizer.EuroAmount(209, 0));
-	var actualResult = BookingOptimizer.optimize(3, 3, clientOffers);
+    @ParameterizedTest
+    @MethodSource("clientTestProvider")
+    void clientTest(
+		     List<BookingOptimizer.EuroAmount> clientOffers,
+		     int freePremiumRooms,
+		     int freeEconomyRooms,
+		     int expectedPremiumRoomsBooked,
+		     BookingOptimizer.EuroAmount expectedPremiumRoomsIncome,
+		     int expectedEconomyRoomsBooked,
+		     BookingOptimizer.EuroAmount expectedEconomyRoomsIncome)
+    {
+	var actualResult = BookingOptimizer.optimize(freePremiumRooms, freeEconomyRooms, clientOffers);
 	var expectedResult = new BookingOptimizer.OptimizationResult(
             new BookingOptimizer.OptimizationResultForPremium(
-                3, new BookingOptimizer.EuroAmount(738, 0)),
+                expectedPremiumRoomsBooked, expectedPremiumRoomsIncome),
             new BookingOptimizer.OptimizationResultForEconomy(
-		3, new BookingOptimizer.EuroAmount(167, 99)));
+                expectedEconomyRoomsBooked, expectedEconomyRoomsIncome));
 	assertEquals(expectedResult, actualResult);
+    }
+    static List<BookingOptimizer.EuroAmount> clientTestOffers =
+                Arrays.asList(
+                    new BookingOptimizer.EuroAmount(23, 0),
+                    new BookingOptimizer.EuroAmount(45, 0),
+                    new BookingOptimizer.EuroAmount(155, 0),
+                    new BookingOptimizer.EuroAmount(374, 0),
+                    new BookingOptimizer.EuroAmount(22, 0),
+                    new BookingOptimizer.EuroAmount(99, 99),
+                    new BookingOptimizer.EuroAmount(100, 0),
+                    new BookingOptimizer.EuroAmount(101, 0),
+                    new BookingOptimizer.EuroAmount(115, 0),
+                    new BookingOptimizer.EuroAmount(209, 0));
+	
+    static Stream<Arguments> clientTestProvider() {
+	return Stream.of(
+            Arguments.of(
+                clientTestOffers,
+                3, 3,
+                3, new BookingOptimizer.EuroAmount(738, 0),
+                3, new BookingOptimizer.EuroAmount(167, 99)),
+            Arguments.of(
+                clientTestOffers,
+                7, 5,
+                6, new BookingOptimizer.EuroAmount(1054, 0),
+                4, new BookingOptimizer.EuroAmount(189, 99)),
+            Arguments.of(
+                clientTestOffers,
+                2, 7,
+                2, new BookingOptimizer.EuroAmount(583, 0),
+                4, new BookingOptimizer.EuroAmount(189, 99)),
+            Arguments.of(
+                clientTestOffers,
+                7, 1,
+                7, new BookingOptimizer.EuroAmount(1153, 99),
+                1, new BookingOptimizer.EuroAmount(45, 0)));
     }
 }
