3a1db62e8140ae03eb5f96aa60ccbafa69cb4bdb
Piotr Koźbiał <piotr.jan.kozbial@gmail.com>
2023-11-13 17:38:07 +0100

    first real test

--- src/test/java/org/example/BookingOptimizerTest.java
+++ src/test/java/org/example/BookingOptimizerTest.java
@@ -1,11 +1,23 @@
 package org.example;
 
-import org.example.BookingOptimizer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 class BookingOptimizerTest {
     @Test
-    void dummy() {
+    void noOffers() {
+	List<BookingOptimizer.EuroAmount> clientOffers = Arrays.asList();
+	var actualResult = BookingOptimizer.optimize(5, 3, clientOffers);
+	var expectedResult = new BookingOptimizer.OptimizationResult(
+            new BookingOptimizer.OptimizationResultForPremium(
+                0, new BookingOptimizer.EuroAmount(0, 0)),
+            new BookingOptimizer.OptimizationResultForEconomy(
+		0, new BookingOptimizer.EuroAmount(0, 0)));
+
+	assertEquals(actualResult, expectedResult);
     }
 }
